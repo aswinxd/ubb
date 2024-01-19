889df250129178fbc28b7110f5f7db7319dd4d72
aswinxd <155294690+aswinxd@users.noreply.github.com>
2024-01-19 17:48:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 class Config:
     API_ID = "25618507"
     API_HASH = "b8b91983b578360ec05a7e88e17b06ea"
-    SESSION_STRING = "my_session"
-    GROUP_DELETE_TIME = 300  # example interval in seconds
-    CHANNEL_DELETE_TIME = 60  # example interval in minutes
-    GROUP_ID = -1002120547379  # replace with your group ID
-    CHANNEL_ID = -1002025718840  # replace with your channel ID
+    SESSION_STRING = "my_session" #dont edit
+    GROUP_DELETE_TIME = 300 
+    CHANNEL_DELETE_TIME = 60  
+    GROUP_ID = -1002120547379  
+    CHANNEL_ID = -1002025718840  
